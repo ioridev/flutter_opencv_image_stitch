da5fd544e40f28a148c6370afcc85767fc2a62e8
iori kojima <iori@iori.dev>
2022-07-24 23:31:15 +0900

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# opencv_flutter_ffi
+
+Uses dart:ffi to run OpenCV C++ 4.5.3 on Flutter App.
+
+Provided release with a sample gaussian blur
+
+Please install OpenCV iOS Framework and put it at location ios/opencv2.framework
+
+## References
+
+- [OpenCV iOS Framework](https://sourceforge.net/projects/opencvlibrary/files/4.5.3/opencv-4.5.3-ios-framework.zip/download)
+- [OpenCV Android Framework](https://sourceforge.net/projects/opencvlibrary/files/4.5.3/opencv-4.5.3-android-sdk.zip/download)
+- [About FFI](https://www.youtube.com/watch?v=2MMK7YoFgaA)
+- [Android config](https://www.flutterclutter.dev/flutter/tutorials/implementing-edge-detection-in-flutter/2020/1509/)
+- [iOS config](https://www.raywenderlich.com/21512310-calling-native-libraries-in-flutter-with-dart-ffi#toc-anchor-015)
+- [Flutter CameraController](https://flutter.dev/docs/cookbook/plugins/picture-using-camera)
+- [Flutter startImageStream](https://pub.dev/documentation/camera/latest/camera/CameraController/startImageStream.html)
+- [Image Format Group](https://github.com/flutter/flutter/issues/26348#issuecomment-796566914)
+
+## Known Issues
+
+- [image_picker crashes when picking images on simulator running on Apple Silicon M1 #71943](https://github.com/flutter/flutter/issues/71943)
+- [Some plugins breaks on release mode on Android #78625](https://github.com/flutter/flutter/issues/78625)
---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:ffi';
+import 'package:ffi/ffi.dart';
+import 'dart:io';
+import 'package:camera/camera.dart';
+
+import 'livecamera.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'OpenCV on Flutter',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(title: 'OpenCV C++ on dart:ffi'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final ImagePicker _picker = ImagePicker();
+  // For Android, you call DynamicLibrary to find and open the shared library
+  // You don’t need to do this in iOS since all linked symbols map when an app runs.
+  final dylib = Platform.isAndroid
+      ? DynamicLibrary.open("libOpenCV_ffi.so")
+      : DynamicLibrary.process();
+  Image _img = Image.asset('assets/img/default.jpg');
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  WidgetsFlutterBinding.ensureInitialized();
+                  // Obtain a list of the available cameras on the device.
+                  final cameras = await availableCameras();
+                  // Get a specific camera from the list of available cameras.
+                  final firstCamera = cameras.first;
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              TakePictureScreen(camera: firstCamera)));
+                },
+                child: Text('Camera')),
+            ElevatedButton(
+              onPressed: () async {
+                final imageFile =
+                    await _picker.pickImage(source: ImageSource.gallery);
+                final imagePath =
+                    imageFile?.path.toNativeUtf8() ?? "none".toNativeUtf8();
+                final gaussian = dylib.lookupFunction<
+                    Void Function(Pointer<Utf8>),
+                    void Function(Pointer<Utf8>)>('Gaussian');
+                gaussian(imagePath);
+                setState(() {
+                  _img = Image.file(File(imagePath.toDartString()));
+                });
+              },
+
+              child: Text("Pick Image from Gallery"),
+            ),
+
+            
+
+
+            Center(child: _img),
+          ],
+        ),
+      ),
+    );
+  }
+}
